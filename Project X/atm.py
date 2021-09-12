420a80cc401819ee9ef53284b7825bac0dfe2ef5
NiladreeBasu <69314575+NiladreeBasu@users.noreply.github.com>
2021-09-12 02:25:25 -0700

    Add files via upload

---
+++ Project X/atm.py
@@ -0,0 +1,19 @@
+def main():
+        card = input("Pls enter your card no. :-")
+        pin = input("Pls enter your pin no. :-")
+
+        amount = input("Pls enter your amount :-")
+
+        print("Withdrawing....")
+
+        crash = input("For completion pls enter your BOD :-")
+
+        print(crash+" ... on this day u will receive your cash Thank You")   
+
+main()
+
+
+    
+
+
+    
